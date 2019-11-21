740d148f00d987e0dab472d56b8113b6d539c5c2
Microyu <gxywy@hotmail.com>
2019-11-21 14:40:23 +0800

    update 11.21

--- logger.py
+++ logger.py
@@ -9,63 +9,67 @@ import time
 import logging
 import matplotlib.pyplot as plt
 import numpy as np
-from tensorboardX import SummaryWriter
 
 class Logger():
-    def __init__(self, save_dir, use_tensorboard=False):
-        self.use_tensorboard = use_tensorboard
+    def __init__(self, exp_name, use_tensorboard=False):
+        self.save_dir = "./logs/" + exp_name
         if not os.path.exists(self.save_dir):
             os.makedirs(self.save_dir)
+        self.csv_file = open(self.save_dir + 'monitor.csv', 'w')
+        self.csv_file.write('r,l,t\n')
+        self.csv_file.flush()
+
         self.step_counter = 0
         self.episode_counter = 0
+        self.steps = []
         self.rewards = []
         self.losses = []
-        self.reward_csv = open(self.save_dir + 'reward.csv', 'w')
-        self.loss_csv = open(self.save_dir + 'loss.csv', 'w')
-        self.is_learning_start = False
 
+        self.use_tensorboard = use_tensorboard
+        self.is_learning_start = False
+        
         logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(levelname)s %(message)s')
+
         if use_tensorboard:
+            from tensorboardX import SummaryWriter
             self.tf_board_writer = SummaryWriter()
-            logging.warn("tensorboardX is enable, please open tensorboard server in current path!")
-        logging.info("RL_Plotter initialized successfully!")
+            logging.warn("tensorboardX is logger enable, please open tensorboard server in current path!")
 
     def add_step(self):
         self.step_counter += 1
     
     def add_episode(self):
+        self.steps.append(self.step_counter)
+        self.step_counter = 0
         self.episode_counter += 1
 
     def add_reward(self, reward, freq=10):
-        self.add_episode()
         self.rewards.append(reward)
+        total_step = np.sum(self.steps)
+
         if self.use_tensorboard:
-            self.tf_board_writer.add_scalar('Train/reward', reward, self.episode_counter)
-        
-        if self.episode_counter % 10 == 0:
-            logging.info("episodes: %d, mean reward: %.2f, steps: %d, mean loss: %f" % \
-                (self.episode_counter, np.mean(self.rewards[-10:]), self.step_counter, np.mean(self.losses[-10])))
+            self.tf_board_writer.add_scalar('Train/reward', reward, total_step)
         
         if self.episode_counter % freq == 0:
-            self.reward_csv.write(str(self.episode_counter) +','+ str(self.rewards[-freq:])+'\n')
-            self.reward_csv.flush()
+            logging.info("episodes: %d, mean reward: %.2f, steps: %d, mean loss: %f" % \
+                (self.episode_counter, np.mean(self.rewards[-freq:]), total_step, np.mean(self.losses[-freq])))
         
-        return self.episode_counter
+        self.csv_file.write(str(self.steps[-1]) +','+ str(reward)+'\n')
+        self.csv_file.flush()
 
     def add_loss(self, loss):
-        self.add_step()
         self.losses.append(loss)
+        total_step = np.sum(self.steps)
+
         if self.use_tensorboard:
-            self.tf_board_writer.add_scalar('Train/loss', loss, self.step_counter)
+            self.tf_board_writer.add_scalar('Train/loss', loss, total_step)
         
         if not self.is_learning_start:
             logging.warn("start learning, loss data received.")
             self.is_learning_start = True
 
-        self.loss_csv.write(str(self.step_counter) +','+ str(loss)+'\n')
-        self.loss_csv.flush()
-
-        return self.step_counter
+        self.csv_file.write(str(total_step) +','+ str(loss)+'\n')
+        self.csv_file.flush()
 
     def reset(self):
         self.episode_counter = 0
